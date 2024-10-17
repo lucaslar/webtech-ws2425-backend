eb76b7502a9e83cc304891edb9955dd75a0a7a05
Lucas Larisch <lucas.larisch@fokus.fraunhofer.de>
2024-10-17 18:28:37 +0200

    Initialized Spring Boot app.

---
+++ src/main/java/de/htwberlin/webtech/webtech_ws2425_backend/WebtechWs2425BackendApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.webtech_ws2425_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechWs2425BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechWs2425BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/webtech_ws2425_backend/WebtechWs2425BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.webtech_ws2425_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechWs2425BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
